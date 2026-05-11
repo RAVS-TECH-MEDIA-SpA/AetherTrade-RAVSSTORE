8e0d97fb42ca5efcda661b4dc31070fb44f47062
Rodrigo Vargas <rodrigovargassanhueza@gmail.com>
2026-05-11 08:36:56 -0400

    eliminacion codigo path dotenv en db.ts

--- apps/landing-next/src/lib/db.ts
+++ apps/landing-next/src/lib/db.ts
@@ -1,15 +1,6 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, '../../../../.env') });
-
 const poolConfig = process.env.DATABASE_URL
   ? { 
       connectionString: process.env.DATABASE_URL,
