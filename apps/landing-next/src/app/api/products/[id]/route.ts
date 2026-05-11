f7f55dbe1f17a446cf7d2c42a5627689a980aca4
Rodrigo Vargas <rodrigovargassanhueza@gmail.com>
2026-05-11 08:32:51 -0400

    ajuste en route, eliminación de código path dotenv

--- apps/landing-next/src/app/api/products/[id]/route.ts
+++ apps/landing-next/src/app/api/products/[id]/route.ts
@@ -1,15 +1,6 @@
 import { NextResponse } from 'next/server';
 import { MercadoPagoConfig, Preference } from 'mercadopago';
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, '../../../../.env') });
-
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ id: string }> }
